6a56054e1e3e6d4b7f5c2cc4194c65df6c6ba147
Mahmud <mahmudsuberu9@gmail.com>
2024-09-15 19:47:25 -0700

    fix: change name from honey gold schools to honey gold model schools

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,10 @@
 const schoolData = {
-  title: "Honey Gold Schools",
+  title: "Honey Gold Model Schools",
   logo: "/logo.png",
 
   // Home Page Starts
   home: {
-    header: { main: "Welcome to", span: "Honey Gold Schools" },
+    header: { main: "Welcome to", span: "Honey Gold Model Schools" },
     paragraph:
       "Lorem ipsum dolor sit amet consectetur. Neque viverra vitae volutpat nulla habitant consequat. Lorem ipsum dolor sit amet consectetur. Neque viverra vitae volutpat nulla habi",
     image: "/home.jpg",
